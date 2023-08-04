16f406655f711bc287c09b58b36881ae3ddfc907
Ayan Das <53610626+AyanDas348@users.noreply.github.com>
2023-08-04 21:08:25 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -119,11 +119,7 @@ export default function Navbar() {
                 <div className="flex items-center justify-between">
                   <Link to="/" className="-m-1.5 p-1.5">
                     <span className="sr-only">Your Company</span>
-                    <img
-                      className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt=""
-                    />
+                    <h1 className='font-bold'>GROW REACH</h1>
                   </Link>
                   <button
                     type="button"
