d4624d1319dd1413c8ccbc9fff7f2721b785f04f
Ayan Das <53610626+AyanDas348@users.noreply.github.com>
2023-08-04 21:12:32 +0530

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,14 +3,16 @@ import SlideUp from '../pages/SlideUp'
 const Services = () => {
   return (
     <section className='bg-section6 bg-cover sm:h-screen'>
-      <div className='sm:w-1/2'></div>
-      <div className="sm:mx-96 mx-10 py-32 sm:py-48 lg:py-56 snap-always sm:my-3">
+      <div className="sm:mx-60 mx-10 sm:max-w-[45%] py-32 sm:py-48 lg:py-56 w-70 snap-always relative">
           <div className="items-start">
-            <h1 className="text-xl font-bold tracking-tight text-white-900 sm:text-8xl">
-              What we offer
+          <p className="mt-6 text-sm sm:text-base leading-8 text-white-600">
+            What we do
+            </p>
+            <h1 className="text-xl font-bold tracking-tight text-white-900 sm:text-6xl py-5">
+            END-TO-END EXCELLENCE
             </h1>
             <p className="mt-6 text-sm sm:text-2xl leading-8 text-white-600">
-            Powerful Digital Marketing Solutions to Propel Your Business Forward
+            We are designed to deliver growth across the entire media spectrum, adapting rapidly to consumer behaviour through a performance mindset.
             </p>
           </div>
         </div>
