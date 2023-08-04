fcec0ceeb7f5d4d87c2586ffbe8b04b18c3f91dc
Ayan Das <53610626+AyanDas348@users.noreply.github.com>
2023-08-04 21:09:33 +0530

    Update Career.jsx

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -4,20 +4,17 @@ const Career = () => {
         <section className='bg-section3 bg-cover'>
           <div className="sm:mx-60 mx-10 sm:max-w-[45%] py-32 sm:py-48 lg:py-56 w-70 snap-always relative">
           <div className="items-start">
-            <h1 className="text-xl font-bold tracking-tight text-white-900 sm:text-8xl">
-            Join Our Dynamic Team and Make a Difference in the Digital World
-
+          <p className="mt-6 text-sm sm:text-base leading-8 text-white-600">
+            Join Us
+            </p>
+            <h1 className="text-xl font-bold tracking-tight text-white-900 sm:text-6xl py-5">
+            WE ARE BORN TO PERFORM, ARE YOU?
             </h1>
             <p className="mt-6 text-sm sm:text-2xl leading-8 text-white-600">
-            We're Looking for Passionate Individuals to Join Us
+            We’re looking for you, dream maker. Do you have a passion for advertising, and always asking “What if?” Come work with people who do good, for all.
             </p>
-            <div className="mt-10 flex items-center gap-x-6">
-              <a href="/career" className="text-sm font-semibold leading-6 text-white-900">
-                Explore Opportunities <span aria-hidden="true">→</span>
-              </a>
             </div>
           </div>
-        </div>
         </section>
   )
 }
